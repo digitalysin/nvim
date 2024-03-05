c1dd32d1e691b87d3684405320ae51a0cd0c6f68
ɐʇɹɐʎpıʍ ɥıןɐƃ <digitalysin@yahoo.co.id>
2024-03-05 17:55:44 +0700

    add jdtls plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,11 +21,6 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local neoconf = require("neoconf")
-
-			-- setup neoconf before lsp config
-			neoconf.setup({})
-
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
@@ -54,6 +49,99 @@ return {
 				single_file_support = true,
 				cmd = { "elixir-ls" },
 			}, {})
+
+			-- setup jdtls
+			local jdtls = require("jdtls")
+			local root_markers = { "gradlew", ".git" }
+			local root_dir = require("jdtls.setup").find_root(root_markers)
+			local home = os.getenv("HOME")
+			local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+
+			lspconfig.jdtls.setup({
+				settings = {
+					java = {
+						signatureHelp = { enabled = true },
+						contentProvider = { preferred = "fernflower" },
+						completion = {
+							favoriteStaticMembers = {
+								"org.hamcrest.MatcherAssert.assertThat",
+								"org.hamcrest.Matchers.*",
+								"org.hamcrest.CoreMatchers.*",
+								"org.junit.jupiter.api.Assertions.*",
+								"java.util.Objects.requireNonNull",
+								"java.util.Objects.requireNonNullElse",
+								"org.mockito.Mockito.*",
+							},
+							filteredTypes = {
+								"com.sun.*",
+								"io.micrometer.shaded.*",
+								"java.awt.*",
+								"jdk.*",
+								"sun.*",
+							},
+						},
+						sources = {
+							organizeImports = {
+								starThreshold = 9999,
+								staticStarThreshold = 9999,
+							},
+						},
+						codeGeneration = {
+							toString = {
+								template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+							},
+							hashCodeEquals = {
+								useJava7Objects = true,
+							},
+							useBlocks = true,
+						},
+						configuration = {
+							runtimes = {
+								{
+									name = "JavaSE-1.8",
+									path = home .. "/.asdf/installs/java/corretto-8.362.08.1",
+								},
+								{
+									name = "JavaSE-11",
+									path = home .. "/.asdf/installs/java/adoptopenjdk-11.0.21+9",
+								},
+								{
+									name = "JavaSE-17",
+									path = home .. "/.asdf/installs/java/adoptopenjdk-17.0.10+7",
+								},
+								{
+									name = "JavaSE-20",
+									path = home .. "/.asdf/installs/java/openjdk-20.0.1",
+								},
+								{
+									name = "JavaSE-21",
+									path = home .. "/.asdf/installs/java/openjdk-21.0.1",
+								},
+							},
+						},
+					},
+				},
+				cmd = {
+					home .. "/.asdf/installs/java/adoptopenjdk-17.0.10+7/bin/java",
+					"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+					"-Dosgi.bundles.defaultStartLevel=4",
+					"-Declipse.product=org.eclipse.jdt.ls.core.product",
+					"-Dlog.protocol=true",
+					"-Dlog.level=ALL",
+					"-Xmx4g",
+					"--add-modules=ALL-SYSTEM",
+					"--add-opens",
+					"java.base/java.util=ALL-UNNAMED",
+					"--add-opens",
+					"java.base/java.lang=ALL-UNNAMED",
+					"-jar",
+					vim.fn.glob(home .. "/.local/share/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+					"-configuration",
+					home .. "/.local/share/jdtls/config_mac",
+					"-data",
+					workspace_folder,
+				},
+			})
 
 			-- to show documentation
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
--- lua/plugins/neoconf.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"folke/neoconf.nvim",
-	config = function()
-		-- keybinding
-		vim.keymap.set("n", "<leader>nl", ":Neoconf local<CR>")
-		vim.keymap.set("n", "<leader>ng", ":Neoconf global<CR>")
-    vim.keymap.set("n", "<leader>ns", ":Neoconf show<CR>")
-	end,
-}
