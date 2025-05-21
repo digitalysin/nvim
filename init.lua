77c1d6380beb83b6d3f326cd3f9550febee9a6f4
ɐʇɹɐʎpıʍ ɥıןɐƃ <galih.widyarta@tiket.com>
2025-05-21 17:06:54 +0700

    add tiny diagnostic

--- init.lua
+++ init.lua
@@ -2,14 +2,14 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local home = os.getenv("HOME")
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -22,15 +22,15 @@ require("configs/mappings")
 vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", { noremap = true })
 
 local neotest_golang_opts = {
-	testify_enabled = true,
+  testify_enabled = true,
 }
 
 require("lspconfig").gleam.setup({})
 require("neotest").setup({
-	adapters = {
-		require("neotest-golang")(neotest_golang_opts),
-		require("neotest-java")({
-			incremental_build = true,
-		}),
-	},
+  adapters = {
+    require("neotest-golang")(neotest_golang_opts),
+    require("neotest-java")({
+      incremental_build = true,
+    }),
+  },
 })
